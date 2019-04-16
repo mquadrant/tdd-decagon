308526405adc63f5add9e30e905af89961a7ff63
Benjamin Mark <mquadrant@gmail.com>
2019-04-16 12:03:17 -0400

    Added a test scenario to test for summation of decimal numbers

--- indefiniteSum.test.js
+++ indefiniteSum.test.js
@@ -3,3 +3,7 @@ const sum = require('./indefiniteSum');
 test("Adding real numbers", () =>{
     expect(sum(3,7,22,4,12)).toBe(48);
 });
+
+test("Adding decimal numbers",() => {
+    expect(sum(2.5,4.1,6.6)).toBeCloseTo(13.2);
+});
