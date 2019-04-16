e386d929daab2012c499dcbcc8844630b3300298
Benjamin Mark <mquadrant@gmail.com>
2019-04-16 12:25:15 -0400

    Added a test case for string inputs

--- indefiniteSum.test.js
+++ indefiniteSum.test.js
@@ -7,3 +7,7 @@ test("Adding real numbers", () =>{
 test("Adding decimal numbers",() => {
     expect(sum(2.5,4.1,6.6)).toBeCloseTo(13.2);
 });
+
+test("One or more of the added number is a string",() =>{
+    expect(sum(a,4.4,b,1.2)).toBe('One or more of the inputs is a string')
+});
