bd90d019d5ca522bc2f6aa82ca9739845d1c1641
Benjamin Mark <mquadrant@gmail.com>
2019-04-16 18:02:55 -0400

    A test case when the second number is greater

--- difference.test.js
+++ difference.test.js
@@ -4,3 +4,8 @@ test('When the first number is greater than the second number', () => {
     expect(diff(6,4)).toBe(2);
     expect(diff(34,21)).toBe(13);
 });
+
+test("when the second number is greater", () => {
+    expect(diff(4,6)).toBe(2);
+    expect(diff(21,34)).toBe(13);
+})
