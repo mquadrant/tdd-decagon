51e892f3619473b160eb560a44b94da0c0d1513d
Benjamin Mark <mquadrant@gmail.com>
2019-04-17 04:31:07 -0400

    developed a function to divide the first number by the second number

--- divideTwo.test.js
+++ divideTwo.test.js
@@ -3,3 +3,7 @@ const divide = require('./divideTwo');
 test('Division when first and the second is a real number',()=>{
     expect(divide(4,2)).toBe(2);
 });
+
+test('Division when the second is greater than the first',()=>{
+    expect(divide(3,6)).toBeCloseTo(0.5);
+});
