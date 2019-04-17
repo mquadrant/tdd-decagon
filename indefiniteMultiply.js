348329a3d65748b9073843252bf7900d14a7b354
Benjamin Mark <mquadrant@gmail.com>
2019-04-17 03:51:03 -0400

    refactored the function to cater for non number type

--- indefiniteMultiply.js
+++ indefiniteMultiply.js
@@ -1,7 +1,8 @@
 const indefiniteMult = (...valueArgs) => {
     let len = valueArgs.length;
-    let result = valueArgs[0];
-    for(let i = 1; i <= len-1; i++){
+    let result = 1;
+    for(let i = 0; i <= len-1; i++){
+        if(typeof valueArgs[i] !== 'number')return 'not valid'
         result *= valueArgs[i];
     } 
     return result;
