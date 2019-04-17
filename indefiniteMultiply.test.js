1c18019eb0c5a276e6367a0f13c5bc180453ace9
Benjamin Mark <mquadrant@gmail.com>
2019-04-17 03:36:48 -0400

    added a test case for multiplication of one or more decimal

--- indefiniteMultiply.test.js
+++ indefiniteMultiply.test.js
@@ -3,3 +3,7 @@
  test('Multiplying all the arguments all together',()=>{
     expect(multiply(3,2,6,2)).toBe(72);
  });
+
+ test('Multiplication when one or more argument is decimal',()=>{
+    expect(multiply(2.1,1.3,5,7,3,2)).toBeCloseTo(573.3);
+ });
