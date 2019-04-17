e1a4c9263f8c3cbac52c8f8851674a83bbf92e0a
Benjamin Mark <mquadrant@gmail.com>
2019-04-17 17:17:38 -0400

    A test case when a repository is empty

--- github-api.test.js
+++ github-api.test.js
@@ -5,10 +5,16 @@ describe('Testing the getReposName function', () => {
     it('should return the users repository name', async () =>{
             expect(await getRepos('mogbeyi-david')).toContain('code-hack')
         });
+
     it('should return the users repository name', async () =>{
             expect(await getRepos('ded')).toContain('express-limiter')
         });
+
     it('When the repository does not exist', async () =>{
             expect(await getRepos('dehg4')).toContain('error')
+        });
+
+    it('When the repository is empty', async () =>{
+            expect(await getRepos('empt')).toContain('error')
         });
 });
