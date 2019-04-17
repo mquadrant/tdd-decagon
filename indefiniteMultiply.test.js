ede70d594451d0c66bbd344e557466d0bbb243d1
Benjamin Mark <mquadrant@gmail.com>
2019-04-17 03:45:56 -0400

    added a test case when one or more arguments is an object

--- indefiniteMultiply.test.js
+++ indefiniteMultiply.test.js
@@ -1,9 +1,13 @@
  const multiply = require('./indefiniteMultiply');
 
- test('Multiplying all the arguments all together',()=>{
+ test('Multiplying all real number arguments all together',()=>{
     expect(multiply(3,2,6,2)).toBe(72);
  });
 
  test('Multiplication when one or more argument is decimal',()=>{
     expect(multiply(2.1,1.3,5,7,3,2)).toBeCloseTo(573.3);
+ });
+
+ test('Multiplication when one or more argument is an object',()=>{
+    expect(multiply(4,{},5,{})).toEqual('not valid');
  });
