f127d2c039e91873f396beed06a1ee226ccf1712
Benjamin Mark <mquadrant@gmail.com>
2019-04-17 16:26:31 -0400

    develop a function that will consume data from github api

--- github-api.js
+++ github-api.js
@@ -1,3 +1,15 @@
+const fetch = require('node-fetch');
+
 function getReposName(userName){
-    
+    const endpoint = `http://api.github.com/users/${userName}/repos`;
+
+    return fetch(endpoint)
+    .then(res => res.json())
+    .then(repositories => {
+        let result = repositories.map(repository => {
+            return repository.name;
+        })
+        return result;
+    })
 }
+module.exports = getReposName;
--- github-api.test.js
+++ github-api.test.js
@@ -1,11 +1,9 @@
 const getRepos = require('./github-api');
 
-describe('Testing the getReposName function',()=>{
-    it('should return the users repository name',()=>{
-        expect(getRepos('ded')).toContain('bonzo');
-    });
+describe('Testing the getReposName function', () => {
 
-    it('should return the users repository name',()=>{
-        expect(getRepos('mogbeyi-david')).toContain('code-hack');
-    });
+    it('should return the users repository name', async () =>{
+            expect(await getRepos('mogbeyi-david')).toContain('code-hack')
+        });
+        
 });
