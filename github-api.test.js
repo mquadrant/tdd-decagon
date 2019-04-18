5747da450387109095a8cc0c3ecf616e52fb1a59
Benjamin Mark <mquadrant@gmail.com>
2019-04-18 04:16:25 -0400

    created a mocks github-api for test purposes

--- github-api.test.js
+++ github-api.test.js
@@ -1,20 +1,21 @@
+jest.mock('./github-api');
 const getRepos = require('./github-api');
 
 describe('Testing the getReposName function', () => {
 
-    it('should return the users repository name', async () =>{
-            expect(await getRepos('mogbeyi-david')).toContain('code-hack')
-        });
+    it('should return the users repository name', async () => {
+        expect(await getRepos('mogbeyi-david')).toContain('code-hack')
+    });
 
-    it('should return the users repository name', async () =>{
-            expect(await getRepos('ded')).toContain('express-limiter')
-        });
+    it('should return the users repository name', async () => {
+        expect(await getRepos('segzy')).toContain('affiliate-giant')
+    });
 
-    it('When the repository does not exist', async () =>{
-            expect(await getRepos('dehg4')).toContain('error')
-        });
+    it('When the repository does not exist', async () => {
+        expect(await getRepos('dehg4')).toContain('error')
+    });
 
-    it('When the repository is empty', async () =>{
-            expect(await getRepos('empt')).toEqual([])
-        });
+    it('When the repository is empty', async () => {
+        expect(await getRepos('empt')).toEqual([]);
+    });
 });
