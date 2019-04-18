6551a4ff8e6dadc48636c7cac11c662295eb5189
Benjamin Mark <mquadrant@gmail.com>
2019-04-18 04:42:14 -0400

    wrote a test case when the first argument is a number

--- concatString.js
+++ concatString.js
@@ -1,4 +1,7 @@
 const concatTwoString = (firstString, secondString) => {
+    if(typeof firstString !== 'string'){
+        return 'error: not a string';
+    }
     return firstString + secondString;
 }
 
--- concatString.test.js
+++ concatString.test.js
@@ -2,9 +2,14 @@ const concat = require('./concatString');
 
 describe('Concatenation when two arguments are strings', ()=>{
     it('When both are strings', () => {
-        expect(concat('this','boy')).toContain('thisboy')
+        expect(concat('this','boy')).toEqual('thisboy');
     });
     it('When one or both are character', () => {
-        expect(concat('T','girl')).toContain('Tgirl')
+        expect(concat('T','girl')).toEqual('Tgirl');
     });
+});
+
+
+test('Concatenation when the first argument is a number',()=>{
+    expect(concat(2,'girl')).toContain('error');
 });
